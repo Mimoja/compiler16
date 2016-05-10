b1029dbd8452e0151bcbe165123c6a6b46a5927d
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2016-05-10 03:14:09 +0200

    common.mk: Zip generation

--- common.mk
+++ common.mk
@@ -3,5 +3,13 @@ all: s$(NUMBER).pdf
 %.pdf: %.md $(EXTRA_GENERATED) $(EXTRA_STATIC)
 	pandoc $< -o $@
 
+ZIP=ex$(NUMBER)_282245_331410_334424_335148.zip
+
+zip: $(ZIP)
+
+$(ZIP): ../w2j all
+	rm -f $(ZIP)
+	cd ../w2j && zip -q $(realpath .)/$(ZIP) -r src tests -i '*.java' -i '*.txt'
+
 clean:
-	rm -f s$(NUMBER).pdf $(EXTRA_GENERATED)
+	rm -f s$(NUMBER).pdf $(EXTRA_GENERATED) $(ZIP)
