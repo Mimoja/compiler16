a1b2efe4d2781c081e19e2f71f8785b3878e12e0
Jonathan Neuschäfer <j.neuschaefer@gmx.net>
2016-05-30 20:39:22 +0200

    reformat RecursiveDescentParserAssignment's comment even more

--- w2j/src/parser/RecursiveDescentParserAssignment.java
+++ w2j/src/parser/RecursiveDescentParserAssignment.java
@@ -9,19 +9,19 @@ import lexer.Symbol;
 /**
  * Recursive descent parser for assignment of the form: int id = num;
  *
- * Grammar: 
- * 1: start -> assignment SEMICOLON EOF
- * 2: assignment -> ID ASSIGN expr
- * 3: expr -> ID subexpr 
- * 4: expr -> NUMBER subexpr 
- * 5: expr -> LPAR expr RPAR 
- * 6: expr -> READ LPAR RPAR subexpr
- * 7: subexpr -> PLUS expr
- * 8: subexpr -> MINUS expr 
- * 9: subexpr -> TIMES expr 
- * 10: subexpr -> DIV expr 
- * 11: subexpr -> MOD expr
- * 12: subexpr -> eps
+ * Grammar:
+ *  1: start      -> assignment SEMICOLON EOF
+ *  2: assignment -> ID ASSIGN expr
+ *  3: expr       -> ID subexpr
+ *  4: expr       -> NUMBER subexpr
+ *  5: expr       -> LPAR expr RPAR
+ *  6: expr       -> READ LPAR RPAR subexpr
+ *  7: subexpr    -> PLUS expr
+ *  8: subexpr    -> MINUS expr
+ *  9: subexpr    -> TIMES expr
+ * 10: subexpr    -> DIV expr
+ * 11: subexpr    -> MOD expr
+ * 12: subexpr    -> eps
  */
 public class RecursiveDescentParserAssignment extends RecursiveDescentParser {
 
