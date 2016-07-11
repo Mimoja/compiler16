cd94161a52b59daaff56aa71e3065194f7dd2ac8
Mimoja <maximilian.schander@rwth-aachen.de>
2016-07-11 12:32:03 +0200

    import e8

--- w2j/src/Main.java
+++ w2j/src/Main.java
@@ -1,8 +1,12 @@
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 import checker.DeclarationChecker;
+import generator.GeneratorException;
+import generator.JasminGenerator;
 import lexer.*;
 import parser.*;
 import parser.grammar.*;
@@ -25,7 +29,7 @@ public class Main {
 
 		String inputProgram = "";
 
-		if (args.length != 1) {
+		if (args.length != 2) {
 			showHelp();
 			System.exit(0);
 		} else {
@@ -38,19 +42,21 @@ public class Main {
 			}
 		}
 
+		String outputFilename = args[1];
+
 		// Append symbol for EOF
 		inputProgram += "$";
 
 		// Lexical Analysis
-
 		List<Symbol> symbols = null;
 		try {
 			symbols = LexerGenerator.analyse(inputProgram);
-			System.out.println("Symbol stream: " + symbols);
+			// System.out.println("Symbol stream: " + symbols);
 		} catch (LexerException e) {
 			System.out.println("LexErr");
 			System.out.println(e.getMessage());
 			System.out.println(e.getAnalysisBeforeFailure());
+			System.exit(1);
 		}
 
 		// Syntactical Analysis
@@ -59,27 +65,49 @@ public class Main {
 		List<Rule> analysis = null;
 		try {
 			analysis = parser.parse(symbols);
-			System.out.println(analysis);
+			// System.out.println(analysis);
 		} catch (ParserException e) {
 			System.out.println("ParseErr");
 			System.out.println(e.getMessage());
 			System.out.println(e.getAnalysisBeforeFailure());
+			System.exit(2);
 		}
 
 		// Semantical Analysis
 		DeclarationChecker checker = new DeclarationChecker(symbols, analysis);
-		// System.out.println(checker.getAst().ast2dot());
-		System.out.println("Every variable was declared before use: " + checker.checkDeclaredBeforeUsed());
+		if (!checker.checkDeclaredBeforeUsed()) {
+			System.out.println("SemanticErr");
+			System.out.println("Not every variable was declared before use.");
+			System.exit(3);
+		}
 
 		// Byte Code Generation
+		JasminGenerator jasminGenerator = new JasminGenerator();
+		String jasminCode = "";
+		try {
+			jasminCode = jasminGenerator.translateWHILE(getFileName(outputFilename), checker.getAst());
+		} catch (GeneratorException e) {
+			System.out.println("GeneratorErr");
+			System.out.println(e.getMessage());
+			System.exit(4);
+		}
+		System.out.println("JASMIN code:");
+		System.out.println(jasminCode);
 
+		try {
+			string2File(outputFilename, jasminCode);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.out.println("Jasmin code written to " + outputFilename);
 	}
 
 	/**
 	 * Show help.
 	 */
 	public static void showHelp() {
-		System.out.println("Usage: java Main PATH_TO_SOURCE_FILE");
+		System.out.println("Usage: java Main PATH_TO_SOURCE_FILE PATH_TO_TARGET_FILE");
 	}
 
 	/**
@@ -107,6 +135,40 @@ public class Main {
 		// Close stream
 		in.close();
 		return str.toString();
+	}
+
+	/**
+	 * Write string to file.
+	 * 
+	 * @param filename
+	 *            Name of file.
+	 * @param s
+	 *            String to write.
+	 * @throws IOException
+	 *             FileNotFoundExeption when the file does not exist or cannot
+	 *             be written to.
+	 */
+	public static void string2File(String filename, String s) throws IOException {
+		FileWriter writer = new FileWriter(filename);
+		writer.write(s);
+		writer.close();
+	}
+
+	/**
+	 * Return filename without extension
+	 * 
+	 * @param file
+	 *            Path to file
+	 * @return Name of the file
+	 */
+	public static String getFileName(String file) {
+		String filename = new File(file).getName();
+		int pos = filename.lastIndexOf(".");
+		if (pos > 0) {
+			return filename.substring(0, pos);
+		} else {
+			return filename;
+		}
 	}
 
 }
--- w2j/src/checker/DeclarationChecker.java
+++ w2j/src/checker/DeclarationChecker.java
@@ -2,13 +2,11 @@ package checker;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 import lexer.Symbol;
 import parser.Rule;
-import symbols.Alphabet;
 import symbols.NonTerminals.NonTerminal;
 import symbols.Tokens.Token;
 
@@ -50,53 +48,75 @@ public class DeclarationChecker {
 	 *         before its declaration.
 	 */
 	public boolean checkDeclaredBeforeUsed() {
-		System.out.println(ast);
+		//System.out.println(ast);
+		try {
+			checkDeclaredness(ast.getRoot(), new HashSet<String>());
+		} catch (VariableNotDeclaredException e) {
+			System.out.println("Undeclared variable " + e.getMessage() + " used! Semantic check failed!");
+			return false;
+		}
+		return true;
+	}
 
-		ASTNode root = ast.getRoot();
-                List<String> varList = new LinkedList<>();
-                
-                return recursiceTraversal(root, varList);
+	/**
+	 * Recursive method for checking the declaredness of identifiers.
+	 * 
+	 * @param node
+	 *            Node to start from.
+	 * @param declaredIDs
+	 *            Set of already declared ids.
+	 * @return Set of declared ids in this AST with root node node.
+	 * @throws VariableNotDeclaredException
+	 *             Thrown when a variable is used but not declared before.
+	 */
+	private Set<String> checkDeclaredness(ASTNode node, Set<String> declaredIDs) throws VariableNotDeclaredException {
+		// base cases:
+		if (node.getAlphabet().equals(NonTerminal.DECLARATION)) {
+			// Case 1: the subtree contains a declaration
+			declaredIDs.add(findDeclaredID(node));
+		} else if (node.getAlphabet() instanceof Token) {
+			// Case 2: the subtree is a leaf. If it is of type ID check whether
+			// it
+			// has been declared before.
+			if (node.getAlphabet().equals(Token.ID)) {
+				// check declaredness
+				if (!declaredIDs.contains(node.getAttribute())) {
+					// Variable was not declared before.
+					throw new VariableNotDeclaredException(node.getAttribute());
+				}
+			}
+		}
+		// other cases: we are in an inner node where information is just passed
+		// up and down, however we must be careful with scopes
+		// Case 3: the subtree is rooted in a new scope, i.e. a while or if
+		// structure
+		// Copy declared IDs into a fresh set, such that IDs that are found
+		// in the subtree are not passed on to upper levels
+		else if (node.getAlphabet().equals(NonTerminal.LOOP)) {
+			Set<String> copyDeclaredIDs = new HashSet<String>();
+			copyDeclaredIDs.addAll(declaredIDs);
+			for (ASTNode subNode : node.getChildren()) {
+				checkDeclaredness(subNode, copyDeclaredIDs);
+			}
+		} else if (node.getAlphabet().equals(NonTerminal.BRANCH)) {
+			Set<String> copyDeclaredIDs = new HashSet<String>();
+			copyDeclaredIDs.addAll(declaredIDs);
+			for (ASTNode subNode : node.getChildren()) {
+				if (subNode.getAlphabet().equals(NonTerminal.PROGRAM)) {
+					copyDeclaredIDs = new HashSet<String>();
+					copyDeclaredIDs.addAll(declaredIDs);
+				}
+				checkDeclaredness(subNode, copyDeclaredIDs);
+			}
+		} else {
+			// Case 4: none of the above applies, just pass on gathered
+			// information, check subtree and pass information up
+			for (ASTNode subNode : node.getChildren()) {
+				checkDeclaredness(subNode, declaredIDs);
+			}
+		}
+		return declaredIDs;
 	}
-        
-        private boolean recursiceTraversal(ASTNode currentN, List<String> declaredAbove){
- 
-            boolean isValid = true;
-            
-            Alphabet currentA = currentN.getAlphabet();
-            
-            // Everything delcared here is valid in the children but not the 
-            // other way round. So we make a temp copy   
-            if(currentA == NonTerminal.LOOP 
-                    || currentA == NonTerminal.BRANCH 
-                    || currentA == NonTerminal.PROGRAM){
-                
-                List<String> currentlyDeclared = new LinkedList<>(declaredAbove);
-                for(ASTNode child : currentN.getChildren()){
-                    isValid = isValid && recursiceTraversal(child, currentlyDeclared);
-                }
-                return isValid;
-            }
-            
-            // current Node is a DELCARATION
-            if(currentA == NonTerminal.DECLARATION){
-                    declaredAbove.add(findDeclaredID(currentN));
-                    return true;
-            }
-            
-            // var is beein used. Has to allready be declared
-            if(currentA == Token.ID
-                    && (!declaredAbove.contains(currentN.getAttribute()))){
-                return false;          
-            }
-            
-            // bridthsearch of the children
-            for(ASTNode child : currentN.getChildren()){
-                isValid = isValid && recursiceTraversal(child, declaredAbove);
-            }
-            
-            return isValid;
-	}
-	
 
 	/**
 	 * Get declared id as attribute of node.
